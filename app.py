381adf97b3753df8ebb50bf384845ec5bb78b423
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-04-06 11:07:39 +0300

    Add Flask forms

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=128)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # You can add the logic to check user credentials here
+        flash('Login requested for user {}, password={}'.format(form.username.data, form.password.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # You can add the logic to store user information here
+        flash('Registration requested for user {}, password={}, email={}'.format(form.username.data, form.password.data, form.email.data))
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
