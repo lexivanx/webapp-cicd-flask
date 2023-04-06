8a06d1676b36c3ba10ae2e445cf1ba5ca16a22f8
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-04-06 11:07:50 +0300

    Update tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,5 +9,34 @@ def client():
 
 def test_hello_world(client):
     rv = client.get('/')
+    assert rv.status_code == 302
+
+def test_login_page(client):
+    rv = client.get('/login')
     assert rv.status_code == 200
-    assert b'Hello, World!' in rv.data
+    assert b'Login' in rv.data
+
+def test_register_page(client):
+    rv = client.get('/register')
+    assert rv.status_code == 200
+    assert b'Register' in rv.data
+
+def test_valid_login(client):
+    rv = client.post('/login', data={'username': 'testuser', 'password': 'testpassword'}, follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'Login requested for user testuser, password=testpassword' in rv.data
+
+def test_invalid_login(client):
+    rv = client.post('/login', data={'username': '', 'password': 'testpassword'})
+    assert rv.status_code == 200
+    assert b'This field is required.' in rv.data
+
+def test_valid_registration(client):
+    rv = client.post('/register', data={'username': 'testuser', 'password': 'testpassword', 'email': 'test@example.com'}, follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'Registration requested for user testuser, password=testpassword, email=test@example.com' in rv.data
+
+def test_invalid_registration(client):
+    rv = client.post('/register', data={'username': 'testuser', 'password': 'testpassword', 'email': 'invalid_email'}, follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'Invalid email address.' in rv.data
